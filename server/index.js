7f428ae10ce629f9bdfdac8b6f276628356f340b
Amruth <amruthharibaskar@gmail.com>
2023-05-29 22:14:21 -0400

    ec2 deploy backend

--- server/index.js
+++ server/index.js
@@ -48,7 +48,8 @@ app.use(
   );
 
 
-const PORT = process.env.PORT || 8080
+//const PORT = process.env.PORT || 8080
+const PORT = 80
 
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`))
